a2c87f71764d0c558e4be2140d6aba473633caa9
Jari Peeperkorn <jari.peeperkorn@gmail.com>
2026-04-15 14:27:12 +0200

    fixed map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,67 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
+    "Achieving Group Fairness through Independence in Predictive Process Monitoring (Vienna, Austria)",
+    48.2082,
+    16.3738
   ],
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
+    "Enhancing Stochastic Petri Net-based Remaining Time Prediction using k-Nearest Neighbors (Bergen, Norway)",
+    60.3913,
+    5.3221
   ],
   [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
+    "Validation Set Sampling Strategies for Predictive Process Monitoring (Krakow, Poland)",
+    50.0647,
+    19.945
   ],
   [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Conformance checking using activity and trace embeddings (Sevilla, Spain)",
+    37.3891,
+    -5.9845
+  ],
+  [
+    "Leveraging manifold learning for adversarial attacks & counterfactuals in process outcome prediction (Copenhagen, Denmark)",
+    55.6761,
+    12.5683
+  ],
+  [
+    "Guest lecture at RWTH Aachen in the Advanced Process Mining course (Aachen, Germany)",
+    50.7753,
+    6.0839
+  ],
+  [
+    "Manifold Learning for Adversarial Robustness in Predictive Process Monitoring (Rome, Italy)",
+    41.9028,
+    12.4964
+  ],
+  [
+    "Process Mining and Predictive Process Monitoring (Leuven, Belgium)",
+    50.8798,
+    4.7005
+  ],
+  [
+    "Supervised Conformance Checking using Recurrent Neural Network Classifiers (Padova, Italy)",
+    45.4064,
+    11.8768
+  ],
+  [
+    "Can deep neural networks learn process model structure? An assessment framework and analysis (Eindhoven, Netherlands)",
+    51.4416,
+    5.4697
+  ],
+  [
+    "Outcome-Oriented Predictive Process Monitoring on Positive and Unlabelled Event Logs (Bolzano, Italy)",
+    46.4983,
+    11.3548
+  ],
+  [
+    "Public Defense (Belgium)",
+    50.5039,
+    4.4699
+  ],
+  [
+    "Process Mining: hoe haal ik zo veel mogelijk uit mijn proces? (Dutch) (Belgium)",
+    50.5039,
+    4.4699
   ]
 ];
